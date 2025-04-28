83ac4a3afa16aec10ab98add315ac529657c2ba9
nelson.chaves@camousucc.edu.co <nelson.chaves@campusucc.edu.co>
2025-04-28 11:59:01 -0500

    Agregando widget shimmer loading

---
+++ lib/app/widgets/shimmer_loading.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class ShimmerLoading extends StatefulWidget {
+  final Widget child;
+  final bool isLoading;
+  final Color baseColor;
+  final Color highlightColor;
+
+  const ShimmerLoading({
+    super.key,
+    required this.child,
+    required this.isLoading,
+    this.baseColor = const Color(0xFFE0E0E0),
+    this.highlightColor = const Color(0xFFF5F5F5),
+  });
+
+  @override
+  State<ShimmerLoading> createState() => _ShimmerLoadingState();
+}
+
+class _ShimmerLoadingState extends State<ShimmerLoading>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat();
+
+    _animation = Tween<double>(begin: -2, end: 2).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOutSine),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!widget.isLoading) return widget.child;
+
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, _) {
+        return ShaderMask(
+          blendMode: BlendMode.srcATop,
+          shaderCallback: (bounds) {
+            return LinearGradient(
+              colors: [
+                widget.baseColor,
+                widget.highlightColor,
+                widget.baseColor,
+              ],
+              stops: const [0.0, 0.5, 1.0],
+              begin: Alignment(_animation.value, 0),
+              end: Alignment(-_animation.value, 0),
+            ).createShader(bounds);
+          },
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
+
+class ShimmerPlaceholder extends StatelessWidget {
+  final double width;
+  final double height;
+  final double borderRadius;
+
+  const ShimmerPlaceholder({
+    super.key,
+    this.width = double.infinity,
+    required this.height,
+    this.borderRadius = 8,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(borderRadius),
+      ),
+    );
+  }
+}
